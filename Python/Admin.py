564c455dbd50e4d1698ffd1a36a384edd3dad911
VamsiPriya-Vemulamada <vamsipiryavemulamada@outlook.com>
2023-06-29 12:49:04 -0400

    Adding code

--- Python/Admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import NewTask
-
-class NewTaskAdmin(admin.ModelAdmin):
-    list_display=("AddTask","time")
-admin.site.register(NewTask,NewTaskAdmin)
--- Python/Models.py
+++
@@ -1,7 +0,0 @@
-from datetime import timezone
-from django.db import models
-import datetime
-# Create your models here.
-class NewTask(models.Model):
-    AddTask=models.CharField(max_length=30)
-    time=models.TimeField(auto_now=False, auto_now_add=False)
